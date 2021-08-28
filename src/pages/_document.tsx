cfe4095bc8f87ddde61c859950259b81efa66cc1
edgarxongas <edgaroboe@outlook.com>
2021-08-27 22:58:11 -0300

    fixed crossOrigin

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -7,7 +7,7 @@ render(){
        <Head>
          <link rel="shortcut icon" href="favicon.png" type="image/png" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link rel="preconnect" href="https://fonts.gstatic.com"/>
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600&family=Rajdhani:wght@600&display=swap" rel="stylesheet" /> 
       </Head>
       <body>
